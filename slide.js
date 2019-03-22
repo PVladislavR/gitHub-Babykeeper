5daf8e7a30f477af1a4e997bcfb33f7ed086fbb5
babykeeper <48771739+PVladislavR@users.noreply.github.com>
2019-03-23 02:17:10 +0300

    apdate slider_1

--- slide.js
+++ slide.js
@@ -1,17 +1,43 @@
-var i = 0, allItems = $('.text').length;
+var i = 0, allItems = $('.text').length, anim=true;
+
+function startTime() {
+    timerId = setInterval(function () {
+        anim=false;
+    }, 1);
+}
+
+function stopTime() {
+    setTimeout(function () {
+        clearInterval(timerId);
+        anim=true;
+    }, 1000);
+}
+
 $('.text').eq(i).css({display: 'flex'});
 
 $('.item_1').bind('click', function () {
-    i++;
-    if(i==allItems){i=0}
-    $('.text').eq(i).css({display: 'flex', left: 100 + '%'})
-    $('.text').eq(i).animate({ left: 0 + '%'}, 1000);
-    $('.text').eq(i-1).animate({ left: -100 + '%'}, 1000);
+    if(anim) {
+        startTime();
+        i++;
+        if (i == allItems) {
+            i = 0
+        }
+        $('.text').eq(i).css({display: 'flex', left: 100 + '%'})
+        $('.text').eq(i).animate({left: 0 + '%'}, 1000);
+        $('.text').eq(i - 1).animate({left: -100 + '%'}, 1000);
+        stopTime();
+    }
 });
 $('.item_3').bind('click', function () {
-    i--;
-    if(i==(allItems * (-1))){i=0}
-    $('.text').eq( i ).css({display: 'flex', left: -100 + '%'});
-    $('.text').eq( i ).animate({ left: 0 + '%'}, 1000);
-    $('.text').eq( i + 1 ).animate({ left: 100 + '%'}, 1000);
+    if(anim) {
+        startTime();
+        i--;
+        if (i == (allItems * (-1))) {
+            i = 0
+        }
+        $('.text').eq(i).css({display: 'flex', left: -100 + '%'});
+        $('.text').eq(i).animate({left: 0 + '%'}, 1000);
+        $('.text').eq(i + 1).animate({left: 100 + '%'}, 1000);
+        stopTime();
+    }
 });
