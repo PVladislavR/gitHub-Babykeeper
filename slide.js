5fbb0ddd87bb33583fdbd624cae85642a88baee4
babykeeper <48771739+PVladislavR@users.noreply.github.com>
2019-03-23 02:10:12 +0300

    Apdate slayder

--- slide.js
+++ slide.js
@@ -1,17 +1,17 @@
 var i = 0, allItems = $('.text').length;
-$('.text').eq(i).css({display: 'block'});
+$('.text').eq(i).css({display: 'flex'});
 
 $('.item_1').bind('click', function () {
     i++;
     if(i==allItems){i=0}
-    $('.text').eq(i).css({display: 'block', left: 100 + '%'})
-    $('.text').eq(i).animate({ left: 0 + '%'}, 500);
-    $('.text').eq(i-1).animate({ left: -100 + '%'}, 500);
+    $('.text').eq(i).css({display: 'flex', left: 100 + '%'})
+    $('.text').eq(i).animate({ left: 0 + '%'}, 1000);
+    $('.text').eq(i-1).animate({ left: -100 + '%'}, 1000);
 });
 $('.item_3').bind('click', function () {
     i--;
     if(i==(allItems * (-1))){i=0}
-    $('.text').eq( i ).css({display: 'block', left: -100 + '%'});
-    $('.text').eq( i ).animate({ left: 0 + '%'}, 500);
-    $('.text').eq( i + 1 ).animate({ left: 100 + '%'}, 500);
+    $('.text').eq( i ).css({display: 'flex', left: -100 + '%'});
+    $('.text').eq( i ).animate({ left: 0 + '%'}, 1000);
+    $('.text').eq( i + 1 ).animate({ left: 100 + '%'}, 1000);
 });
