07495e34d33757441943af19f108ca0e32eeca0e
babykeeper <48771739+PVladislavR@users.noreply.github.com>
2019-03-23 06:58:36 +0300

    menu

--- slide.js
+++ slide.js
@@ -1,3 +1,5 @@
+/*======= Slider ====================================*/
+
 var i = 0, allItems = $('.text').length, anim=true;
 
 function startTime() {
@@ -6,11 +8,11 @@ function startTime() {
     }, 1);
 }
 
-function stopTime() {
+function stopTime(tim) {
     setTimeout(function () {
         clearInterval(timerId);
         anim=true;
-    }, 1000);
+    }, tim);
 }
 
 $('.text').eq(i).css({display: 'flex'});
@@ -22,10 +24,10 @@ $('.item_1').bind('click', function () {
         if (i == allItems) {
             i = 0
         }
-        $('.text').eq(i).css({display: 'flex', left: 100 + '%'})
+        $('.text').eq(i).css({display: 'flex', left: 100 + '%'});
         $('.text').eq(i).animate({left: 0 + '%'}, 1000);
         $('.text').eq(i - 1).animate({left: -100 + '%'}, 1000);
-        stopTime();
+        stopTime(1000);
     }
 });
 $('.item_3').bind('click', function () {
@@ -38,6 +40,66 @@ $('.item_3').bind('click', function () {
         $('.text').eq(i).css({display: 'flex', left: -100 + '%'});
         $('.text').eq(i).animate({left: 0 + '%'}, 1000);
         $('.text').eq(i + 1).animate({left: 100 + '%'}, 1000);
-        stopTime();
+        stopTime(1000);
     }
 });
+
+/*===================================================================*/
+
+
+/*======= Menu =======================================================*/
+var butClick = true;
+
+$(document).bind('wheel', function () {
+    if(!butClick){
+        $('.menu').animate({width: 0 + 'px'}, 1000);
+        butClick=true;
+    }
+});
+
+
+$('.button-menu').bind('click', function () {
+    if(butClick) {
+        $('.menu').animate({width: 60 + 'px'}, 1000);
+        butClick=false;
+    }
+    else
+    {
+        $('.menu').animate({width: 0 + 'px'}, 1000);
+        butClick=true;
+    }
+});
+
+
+$('.menu-items').hover(function () {
+    countItems = $($(this).children(0))[0].children.length;
+    $(this).children(0).css({width: 300 + '%'});
+    $(this).children(0).animate({height: countItems*100 + '%'}, 500)
+    $('.items_in_item_in_item-menu').css({
+        height: 100/countItems + '%'
+    });
+    itemInItemMenu = $(this).children(0);
+    $(itemInItemMenu).hover(function () {
+        $(this).children(0).css({
+            background: 'blue'
+        })
+    }, function () {
+        $(this).children(0).css({
+            background: 'aqua'
+        })
+    })
+},
+    function () {
+        vb = $($(this).children(0))[0].children.length;
+        $(this).children(0).css({
+            width: 0 + '%',
+            height: 0 + '%'
+        });
+        $('.items_in_item_in_item-menu').css({
+            height: 0 + '%'
+        })
+    });
+
+
+
+/*====================================================================*/
