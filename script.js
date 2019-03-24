3c8492b084fd96133d3ee02ec2527a5daa7f6c14
Cerberrus <48838008+Cerberrus@users.noreply.github.com>
2019-03-24 20:50:28 +0300

    Slider

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
- 
-$(document).ready(function(){ 
-    var item = document.getElementsByClassName('section-sixth__slider_item');
-        var dot = document.getElementsByClassName('section-sixth__slider_dot');
-        var i = 1;
+var i = 1, 
+    item = document.getElementsByClassName('section-sixth__slider_item'), 
+    dot = document.getElementsByClassName('section-sixth__slider_dot'),
+    anim=true;
+    
+    function startTime() {
+        timerId = setInterval(function () {
+            anim=false;
+        }, 1);
+    }
 
-        $('#section-sixth__slider_btn-next').on('click', function(){
+    function stopTime(tim) {
+        setTimeout(function () {
+            clearInterval(timerId);
+            anim=true;
+        }, tim);
+    }
+    
+    $('#section-sixth__slider_btn-next').on('click', function () {
+         if(anim) {
+            startTime();
             $(dot[i]).removeClass('active');
-            $(item[i]).removeClass('active');
             i++;
             if(i == item.length){
                 i=0;
             }
             $(dot[i]).addClass('active');
-            $(item[i]).addClass('active');н
-        });
-
-        $('#section-sixth__slider_btn-prev').on('click', function(){
+            $('.section-sixth__slider_item').eq(i).css({left: 100 + '%'});
+            $('.section-sixth__slider_item').eq(i).animate({left: 0 + '%'}, 1000);
+            $('.section-sixth__slider_item').eq(i-1).animate({left: -100 + '%'}, 1000);
+            stopTime(1000);
+         }
+    });    
+    
+    $('#section-sixth__slider_btn-prev').on('click', function () {
+         if(anim) {
+            startTime();
             $(dot[i]).removeClass('active');
-            $(item[i]).removeClass('active');
             i--;
             if(i < 0){
-                i=item.length-1;
+                i=2;
             }
-            $(item[i]).addClass('active');
             $(dot[i]).addClass('active');
-        });
-
-        $('.section-sixth__slider_dot').on('click', function(e){
-            e.preventDefault();
-            $(item[i]).removeClass('active');
-            $(dot[i]).removeClass('active');
+            $('.section-sixth__slider_item').eq(i).css({left: -100 + '%'});
+            $('.section-sixth__slider_item').eq(i).animate({left: 0 + '%'}, 1000);
+            if(i==item.length-1){
+                $('.section-sixth__slider_item').eq(0).animate({left: 100 + '%'}, 1000);
+            }
+            else{
+                $('.section-sixth__slider_item').eq(i+1).animate({left: 100 + '%'}, 1000);
+            }
+            stopTime(1000);
+         }
+    });
+    
+    $('.section-sixth__slider_dot').on('click', function(){
+        if(anim) {
+            startTime();
+            $(dot).eq(i).removeClass('active');
+            var j=i;
             i=$(this).index();
-            $(dot[i]).addClass('active');
-            $(item[i]).addClass('active');
-        });
+            if(i>j){ 
+                $('.section-sixth__slider_item').eq(i).css({left: 100 + '%'});
+                $('.section-sixth__slider_item').eq(i).animate({left: 0 + '%'}, 1000);
+                $('.section-sixth__slider_item').eq(j).animate({left: -100 + '%'}, 1000);
+            }
+            else{
+                $('.section-sixth__slider_item').eq(i).css({left: -100 + '%'});
+                $('.section-sixth__slider_item').eq(i).animate({left: 0 + '%'}, 1000);
+                if(i==item.length-1){
+                    $('.section-sixth__slider_item').eq(0).animate({left: 100 + '%'}, 1000);
+                }
+                else{
+                    $('.section-sixth__slider_item').eq(j).animate({left: 100 + '%'}, 1000);
+                }
+            }
+            $(dot).eq(i).addClass('active');
+            stopTime(1000);
+         }
     });
